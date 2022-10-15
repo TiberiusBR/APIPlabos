6fb50f80cd8c0cb38b5c3f8fc6ab7b8485e50ab1
Tiberius <souza.freire@ftc.edu.br>
2022-10-15 15:47:44 -0300

    feat: Adiciona rota de video stream como exemplo

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.security import OAuth2PasswordBearer
 from app.config.conn import metadata, engine, database
 from app.routes.categoryRoutes import category_router
+from app.routes.videoRoutes import video_router
 
 metadata.create_all(engine)
 
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app.include_router(category_router)
+app.include_router(video_router)
 
 
 @app.on_event("startup")
